17bb5b1564a43ce83e61143f78472d6a391c6d77
JobasStuff <jparaujo0409@gmail.com>
2026-08-23 13:29:38 -0300

    feat(backend): adiciona métodos de salvar e listar no repository

--- backend/src/main/java/com/desafiorh/backend/repository/FuncionarioRepository.java
+++ backend/src/main/java/com/desafiorh/backend/repository/FuncionarioRepository.java
@@ -6,4 +6,12 @@ import java.util.ArrayList;
 
 public class FuncionarioRepository {
     ArrayList<Funcionario> funcionarios = new ArrayList<>();
+
+    public void salvar(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
+    public ArrayList<Funcionario> listar() {
+        return funcionarios;
+    }
+
 }
---
+++ backend/src/main/java/com/desafiorh/backend/service/FuncionarioService.java
@@ -0,0 +1,12 @@
+package com.desafiorh.backend.service;
+
+import com.desafiorh.backend.repository.FuncionarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FuncionarioService {
+    private final FuncionarioRepository repository;
+
+}
