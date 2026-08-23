2114e7ca993704f558b895adc5a5179f74f80a23
JobasStuff <jparaujo0409@gmail.com>
2026-08-23 13:23:19 -0300

    feat(backend): adiciona repository sendo um arraylist

---
+++ backend/src/main/java/com/desafiorh/backend/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.desafiorh.backend.repository;
+
+import com.desafiorh.backend.model.Funcionario;
+
+import java.util.ArrayList;
+
+public class FuncionarioRepository {
+    ArrayList<Funcionario> funcionarios = new ArrayList<>();
+}
