ddfee36eaa0b9ada2046960f31f9e0e4ae144c73
JobasStuff <jparaujo0409@gmail.com>
2026-08-23 15:01:16 -0300

    feat(backend): adiciona @Valid no controller

--- backend/src/main/java/com/desafiorh/backend/controller/FuncionarioController.java
+++ backend/src/main/java/com/desafiorh/backend/controller/FuncionarioController.java
@@ -3,6 +3,7 @@ package com.desafiorh.backend.controller;
 import com.desafiorh.backend.dto.request.FuncionarioRequestDTO;
 import com.desafiorh.backend.dto.response.FuncionarioResponseDTO;
 import com.desafiorh.backend.service.FuncionarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class FuncionarioController {
     private final FuncionarioService service;
 
     @PostMapping
-    public FuncionarioResponseDTO criar(@RequestBody FuncionarioRequestDTO funcionarioRequestDTO) {
+    public FuncionarioResponseDTO criar(@RequestBody @Valid FuncionarioRequestDTO funcionarioRequestDTO) {
         return service.criar(funcionarioRequestDTO);
     }
 }
--- backend/src/main/java/com/desafiorh/backend/repository/FuncionarioRepository.java
+++ backend/src/main/java/com/desafiorh/backend/repository/FuncionarioRepository.java
@@ -1,9 +1,10 @@
 package com.desafiorh.backend.repository;
 
 import com.desafiorh.backend.model.Funcionario;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-
+@Repository
 public class FuncionarioRepository {
     ArrayList<Funcionario> funcionarios = new ArrayList<>();
     Integer contador = 0;
