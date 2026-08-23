c538a773bd8e8ea16ee2349e15b8ae1156505541
JobasStuff <jparaujo0409@gmail.com>
2026-08-23 14:54:41 -0300

    feat(backend): adiciona endpoint post

---
+++ backend/src/main/java/com/desafiorh/backend/controller/FuncionarioController.java
@@ -0,0 +1,22 @@
+package com.desafiorh.backend.controller;
+
+import com.desafiorh.backend.dto.request.FuncionarioRequestDTO;
+import com.desafiorh.backend.dto.response.FuncionarioResponseDTO;
+import com.desafiorh.backend.service.FuncionarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/funcionario")
+@RequiredArgsConstructor
+public class FuncionarioController {
+    private final FuncionarioService service;
+
+    @PostMapping
+    public FuncionarioResponseDTO criar(@RequestBody FuncionarioRequestDTO funcionarioRequestDTO) {
+        return service.criar(funcionarioRequestDTO);
+    }
+}
