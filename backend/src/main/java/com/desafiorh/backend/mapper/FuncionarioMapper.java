57ebe9e679b9e432a36c0fbe50de35d96ebcfdf3
JobasStuff <jparaujo0409@gmail.com>
2026-08-23 14:50:27 -0300

    feat(backend): adiciona lógica de conversão no service

--- backend/src/main/java/com/desafiorh/backend/mapper/FuncionarioMapper.java
+++ backend/src/main/java/com/desafiorh/backend/mapper/FuncionarioMapper.java
@@ -1,6 +1,7 @@
 package com.desafiorh.backend.mapper;
 
 import com.desafiorh.backend.dto.request.FuncionarioRequestDTO;
+import com.desafiorh.backend.dto.response.FuncionarioResponseDTO;
 import com.desafiorh.backend.model.Funcionario;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +10,29 @@ public class FuncionarioMapper {
     public Funcionario toFuncionario(FuncionarioRequestDTO funcionarioRequestDTO) {
         Funcionario funcionario = new Funcionario();
 
+        funcionario.setNome(funcionarioRequestDTO.nome());
+        funcionario.setEmail(funcionarioRequestDTO.email());
+        funcionario.setTelefone(funcionarioRequestDTO.telefone());
+        funcionario.setCargo(funcionarioRequestDTO.cargo());
+        funcionario.setDepartamento(funcionarioRequestDTO.departamento());
+        funcionario.setSalario(funcionarioRequestDTO.salario());
+        funcionario.setCidade(funcionarioRequestDTO.cidade());
+        funcionario.setStatus(funcionarioRequestDTO.status());
+
+        return funcionario;
+    }
+
+    public FuncionarioResponseDTO toFuncionarioResponseDTO(Funcionario funcionario) {
+        return new FuncionarioResponseDTO(
+            funcionario.getId(),
+            funcionario.getNome(),
+            funcionario.getEmail(),
+            funcionario.getTelefone(),
+            funcionario.getCargo(),
+            funcionario.getDepartamento(),
+            funcionario.getSalario(),
+            funcionario.getCidade(),
+            funcionario.getStatus()
+        );
     }
 }
--- backend/src/main/java/com/desafiorh/backend/service/FuncionarioService.java
+++ backend/src/main/java/com/desafiorh/backend/service/FuncionarioService.java
@@ -2,6 +2,7 @@ package com.desafiorh.backend.service;
 
 import com.desafiorh.backend.dto.request.FuncionarioRequestDTO;
 import com.desafiorh.backend.dto.response.FuncionarioResponseDTO;
+import com.desafiorh.backend.mapper.FuncionarioMapper;
 import com.desafiorh.backend.model.Funcionario;
 import com.desafiorh.backend.repository.FuncionarioRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FuncionarioService {
     private final FuncionarioRepository repository;
+    private final FuncionarioMapper FuncionarioMapper;
 
     public FuncionarioResponseDTO criar(FuncionarioRequestDTO funcionarioRequestDTO){
-
+        Funcionario funcionario = FuncionarioMapper.toFuncionario(funcionarioRequestDTO);
+        funcionario.setId(repository.geradorId());
+        repository.salvar(funcionario);
+        return FuncionarioMapper.toFuncionarioResponseDTO(funcionario);
     }
 }
